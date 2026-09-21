62ee10a3c91f615dd17b15ce6b6ea6a7e47e179a
HaeJun <haejunbag131@gmail.com>
2026-09-21 11:34:02 +0900

    refactor: 인증 상태 전역 Provider 구조로 개선

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { AuthProvider } from "@/features/auth/hooks/useAuthUser";
 import { ThemeProvider } from "@/shared/ui/ThemeProvider";
 import { ToastProvider } from "@/shared/ui/ToastProvider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider>
-      {children}
-      <ToastProvider />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        {children}
+        <ToastProvider />
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
